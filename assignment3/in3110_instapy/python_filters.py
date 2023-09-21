6c28aaca5d0589ad902e54f1ba6d2b54495b6c65
Erling Holte <erlinhol@uio.no>
2023-09-21 17:19:52 +0200

    Implement python_color2gray

--- assignment3/in3110_instapy/python_filters.py
+++ assignment3/in3110_instapy/python_filters.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import numpy as np
-
+from PIL import Image
 
 def python_color2gray(image: np.array) -> np.array:
     """Convert rgb pixel array to grayscale
@@ -15,6 +15,18 @@ def python_color2gray(image: np.array) -> np.array:
     gray_image = np.empty_like(image)
     # iterate through the pixels, and apply the grayscale transform
     
+    # The grayscale transform will have the weights 0.21, 0.72 and 0.07
+    shape = gray_image.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            pixel = gray_image[i][j]
+            pixel_real = image[i][j]
+            gray_scale = pixel_real[0] * 0.21 + pixel_real[1] * 0.72 + pixel_real[2] * 0.07
+            pixel[0] = gray_scale
+            pixel[1] = gray_scale
+            pixel[2] = gray_scale
+
+    gray_image = gray_image.astype("uint8")
     return gray_image
 
 
