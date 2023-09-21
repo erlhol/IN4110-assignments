5b6e8c8e6d6c1e6210308189148f4b4dd9090f13
Erling Holte <erlinhol@uio.no>
2023-09-21 18:13:51 +0200

    Solve numba_filters

--- assignment3/in3110_instapy/numba_filters.py
+++ assignment3/in3110_instapy/numba_filters.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import numpy as np
 from numba import jit
 
-
+@jit(nopython=True)
 def numba_color2gray(image: np.array) -> np.array:
     """Convert rgb pixel array to grayscale
 
@@ -15,8 +15,17 @@ def numba_color2gray(image: np.array) -> np.array:
     """
     gray_image = np.empty_like(image)
     # iterate through the pixels, and apply the grayscale transform
+    shape = gray_image.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            pixel = gray_image[i][j]
+            pixel_real = image[i][j]
+            gray_scale = pixel_real[0] * 0.21 + pixel_real[1] * 0.72 + pixel_real[2] * 0.07
+            pixel[0] = gray_scale
+            pixel[1] = gray_scale
+            pixel[2] = gray_scale
 
-    ...
+    gray_image = gray_image.astype("uint8")
     return gray_image
 
 
