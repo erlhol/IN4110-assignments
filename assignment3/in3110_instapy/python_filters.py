c916382a0188585d7fc26772e1eb985a81c5fcb1
Erling Holte <erlinhol@uio.no>
2023-09-23 10:46:02 +0200

    Add python filter

--- assignment3/in3110_instapy/python_filters.py
+++ assignment3/in3110_instapy/python_filters.py
@@ -41,8 +41,22 @@ def python_color2sepia(image: np.array) -> np.array:
     # Iterate through the pixels
     # applying the sepia matrix
 
-    ...
+    sepia_matrix = [
+    [ 0.393, 0.769, 0.189], # R
+    [ 0.349, 0.686, 0.168], # G
+    [ 0.272, 0.534, 0.131], # B
+    ]
+
+    shape = sepia_image.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            pixel = sepia_image[i][j]
+            pixel_real = image[i][j]
+            pixel[0] = min(255,pixel_real[0] * sepia_matrix[0][0] + pixel_real[1] * sepia_matrix[0][1] + pixel_real[2] * sepia_matrix[0][2])
+            pixel[1] = min(255,pixel_real[0] * sepia_matrix[1][0] + pixel_real[1] * sepia_matrix[1][1] + pixel_real[2] * sepia_matrix[1][2])
+            pixel[2] = min(255,pixel_real[0] * sepia_matrix[2][0] + pixel_real[1] * sepia_matrix[2][1] + pixel_real[2] * sepia_matrix[2][2])
 
     # Return image
     # don't forget to make sure it's the right type!
+    sepia_image = sepia_image.astype("uint8")
     return sepia_image
