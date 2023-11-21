d37996892c5b9272516442acc552eb7af2c48afc
Erling Holte <erlinhol@uio.no>
2023-11-21 18:44:40 +0100

    Solve task 5.1

--- assignment5/strompris.py
+++ assignment5/strompris.py
@@ -33,15 +33,46 @@ def fetch_day_prices(date: datetime.date = None, location: str = "NO1") -> pd.Da
     ...
     """
     if date is None:
-        date = ...
+        date = datetime.datetime.now()
 
-    raise NotImplementedError("Remove me when you implement this task")
-    url = ...
-    ...
+    # construct the date format for the GET-request
+    date_format = (
+        str(date.year) + "/" + zero_pad(str(date.month)) + "-" + zero_pad(str(date.day))
+    )
+
+    # Construct the url with the date format and location
+    url = (
+        f"https://www.hvakosterstrommen.no/api/v1/prices/{date_format}_{location}.json"
+    )
+    # Make a GET-request
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+
+    # Parse the get request to json-format
+    data = r.json()
+    df = pd.DataFrame.from_dict(data)
+
+    # Filter out to only include NOK_per_kWh and time_start
+    df = df[["NOK_per_kWh", "time_start"]]
+
+    # Convert to correct timezone (from write-up)
+    df["time_start"] = pd.to_datetime(df["time_start"], utc=True).dt.tz_convert(
+        "Europe/Oslo"
+    )
+    # Convert to float if not already
+    df["NOK_per_kWh"] = df["NOK_per_kWh"].astype("float")
+    return df
 
 
 # LOCATION_CODES maps codes ("NO1") to names ("Oslo")
-LOCATION_CODES = {}
+LOCATION_CODES = {
+    "NO1": "Oslo",
+    "NO2": "Kristiansand",
+    "NO3": "Trondheim",
+    "NO4": "Tromsø",
+    "NO5": "Bergen",
+}
 
 # task 1:
 
@@ -56,12 +87,23 @@ def fetch_prices(
     Make sure to document arguments and return value...
     ...
     """
-    raise NotImplementedError("Remove me when you implement this task")
 
     if end_date is None:
-        end_date = ...
+        end_date = datetime.datetime.now()
 
-    ...
+    total_df = None
+    # traverse in reverse order, from the oldest to the most recent day
+    for n in range(days - 1, -1, -1):
+        for location in locations:
+            td = end_date - datetime.timedelta(n)
+            df = fetch_day_prices(td, location)
+            df["location_code"] = location
+            df["location"] = LOCATION_CODES[location]
+            if total_df is None:
+                total_df = df
+                continue
+            total_df = pd.concat([total_df, df])
+    return total_df
 
 
 # task 5.1:
@@ -76,7 +118,12 @@ def plot_prices(df: pd.DataFrame) -> alt.Chart:
 
     Make sure to document arguments and return value...
     """
-    ...
+    # TODO!
+    return (
+        alt.Chart(df)
+        .mark_line()
+        .encode(x="time_start", y="NOK_per_kWh", color="location")
+    )
 
 
 # Task 5.4
@@ -115,6 +162,17 @@ def plot_activity_prices(
     raise NotImplementedError("Remove me when you implemnt this optional task")
 
     ...
+
+
+def zero_pad(n: str):
+    """zero-pad a number string
+
+    turns '2' into '02'
+
+    """
+    if len(n) == 2:
+        return n
+    return "0" + n
 
 
 def main():
